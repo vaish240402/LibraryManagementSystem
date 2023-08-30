ca5a4752d59c35bb00320e99e24624c2a7a91629
vaish240402 <143653382+vaish240402@users.noreply.github.com>
2023-08-31 01:53:30 +0530

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,31 @@
+package system;
+public class Book {
+    private String title;
+    private String author;
+    private int publicationYear;
+    public Book(String title, String author, int publicationYear) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+               "title='" + title + '\'' +
+               ", author='" + author + '\'' +
+               ", publicationYear=" + publicationYear +
+               '}';
+    }
+}
